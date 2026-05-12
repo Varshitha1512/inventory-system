0bbc509f46cc53066cfcd5505994e8220f5404c2
Varshitha Vattikuti <varshu.vattikuti@gmail.com>
2026-05-13 05:28:16 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -80,8 +80,8 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=False,
 )
-
 
 # ── Models ────────────────────────────────────────────────────────────────────
 
